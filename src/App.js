c40befaffd8d2b939770c963aa8670215667da07
Anton Veldanov <anton.veldanov@gmail.com>
2019-06-15 21:20:59 -0700

    axios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+// import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  componentDidMount() {
+
+
+
+
+  }
+
+
+
+  render() {
+
+    return (
+      <div className="App" >
+        <Navbar />
+        <div className="container">
+          <Users />
+        </div>
+
+
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+// Destructure props
+const Navbar = ({ icon, title }) => {
+
+
+
+
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}</h1>
+    </nav>
+  )
+
+}
+
+
+
+
+Navbar.defaultProps = {
+  title: "Default Title",
+  icon: 'fab fa-github'
+}
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+}
+
+
+export default Navbar
